ded223b0922cdf9c67d6f0edea6c907ef51f18ab
Laurian Gridinoc <laurian@gridinoc.name>
2014-11-20 11:36:11 +0300

    3 more arabic urls

--- src/scripts/ports/hyperaudio/ha-data.js
+++ src/scripts/ports/hyperaudio/ha-data.js
@@ -28,15 +28,21 @@ var VIDEOS = {
 
 [
 ",http://player.vimeo.com/external/109676100.hd.mp4?s=5f80e6a80c367b54273be5918a0eca3a",
+
 "http://www.youtube.com/watch?v=HomYG95MO3k,http://player.vimeo.com/external/109585525.hd.mp4?s=dbcb3c3e779426f92f5ef51f64acbba5",
 "http://www.youtube.com/watch?v=TwJHG2KSsG0,http://player.vimeo.com/external/110884059.hd.mp4?s=3eb6e351f73ca929189cbf1e2d74c8c4",
 "http://www.youtube.com/watch?v=oy4PWG6LCAs,http://player.vimeo.com/external/109585524.hd.mp4?s=988c547e4e5d25236d5862976af9e836",
-"http://www.youtube.com/watch?v=2JqmcqkIrFE,http://player.vimeo.com/external/110467198.hd.mp4?s=2f954a2e824e152bc3bf29a616511b92",
+
+
+
+"http://www.youtube.com/watch?v=2JqmcqkIrFE,http://player.vimeo.com/external/110699967.hd.mp4?s=a10fe4f950e8a0261518bfdf77af6f11",
+
 "http://www.youtube.com/watch?v=t657lfeIg4s,http://player.vimeo.com/external/109585526.hd.mp4?s=1fe0282cf14b8ae2bd82f59f21ad43cb",
 "http://www.youtube.com/watch?v=Jp2UcV2Ldaw,http://player.vimeo.com/external/110467201.hd.mp4?s=53965bd7c092c753d3ab3506273bb5c4",
 "http://youtu.be/a9rGEPGpDis               ,http://player.vimeo.com/external/110467206.hd.mp4?s=d529d60db0e6b9374f75301837c6580e",
 "http://www.youtube.com/watch?v=3dBBBnxKgeQ,http://player.vimeo.com/external/110888355.hd.mp4?s=9360b21d5cb36f15bed0353a02b0d044",
 "http://www.youtube.com/watch?v=GHcFqNICoJM,http://player.vimeo.com/external/110889321.hd.mp4?s=05d019878de216dffe78c98e332bcd52",
+
 "http://www.youtube.com/watch?v=sT22bwJ55Sw,",
 "http://www.youtube.com/watch?v=sXDngNEqdnA,",
 "http://www.youtube.com/watch?v=iOoW9-gUCDw,http://player.vimeo.com/external/110697549.hd.mp4?s=cee08621e19cb94e50df5997d25620b0",
@@ -46,7 +52,11 @@ var VIDEOS = {
 "http://www.youtube.com/watch?v=u49jwfcLwuE,http://player.vimeo.com/external/110467238.hd.mp4?s=1d8125fcb86fbc884d8e7ab2d71aa9d5",
 "http://www.youtube.com/watch?v=H7FML0wzJ6A,",
 "http://www.youtube.com/watch?v=yI2D5Fsd9lg,",
-"http://www.youtube.com/watch?v=5SKECszemmA,",
+
+
+
+"http://www.youtube.com/watch?v=5SKECszemmA,http://player.vimeo.com/external/111081703.hd.mp4?s=921d9fc2812bad401c8662ffa95ab3e8",
+
 "http://www.youtube.com/watch?v=0m__A7MlDrk,"
 ]
 
