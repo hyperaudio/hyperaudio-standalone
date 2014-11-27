a8bfd8b033834cdaa2463c28531f56d9e2d27584
Laurian Gridinoc <laurian@gridinoc.name>
2014-11-27 23:00:09 +0300

    hash, search

--- src/scripts/lang/T/lang.js
+++ src/scripts/lang/T/lang.js
@@ -1,2 +1,2 @@
 window.L = 'T';
-if (window.parent == window && window.location.hash != '') {window.top.location.hash = window.location.hash;}
+if (window.top != window && window.location.hash != '') {window.top.location.hash = window.location.hash;}
