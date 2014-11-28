ea385d4abcea3bfb22c1706c679c4207c06672ce
Laurian Gridinoc <laurian@gridinoc.name>
2014-11-28 16:12:25 +0300

    console.log FFS

--- src/scripts/lang/A/lang.js
+++ src/scripts/lang/A/lang.js
@@ -1,4 +1,25 @@
 window.L = 'A';
 if (window.top != window && window.location.hash != '') {window.top.location.hash = window.location.hash;}
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
 
  
