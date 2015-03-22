49d30016c5af3f6b9da048c5f5041fa269ad555e
Laurian Gridinoc <laurian@gridinoc.name>
2015-03-22 05:59:02 -0300

    meh, git does not preserve permissions

--- stage.sh
+++ stage.sh
@@ -1,3 +1,4 @@
+chmod 600 .ssh/id_rsa
 scp -r -i .ssh/id_rsa mobile-E/* www-data@aljazeera.io:/var/www/html/aje/PalestineRemix/mobile/
 scp -r -i .ssh/id_rsa mobile-A/* www-data@aljazeera.io:/var/www/html/aja/PalestineRemix/mobile/
 scp -r -i .ssh/id_rsa mobile-B/* www-data@aljazeera.io:/var/www/html/ajb/PalestineRemix/mobile/
