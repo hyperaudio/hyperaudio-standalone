7c7cb761b706477b3214c9d544e5caff773bf950
Laurian Gridinoc <laurian@gridinoc.name>
2015-03-22 06:36:44 -0300

    clean-up

--- stage.sh
+++ stage.sh
@@ -1,8 +1,4 @@
 chmod 600 .ssh/id_rsa
-# scp -r -i .ssh/id_rsa mobile-E/* www-data@aljazeera.io:/var/www/html/aje/PalestineRemix/mobile/
-# scp -r -i .ssh/id_rsa mobile-A/* www-data@aljazeera.io:/var/www/html/aja/PalestineRemix/mobile/
-# scp -r -i .ssh/id_rsa mobile-B/* www-data@aljazeera.io:/var/www/html/ajb/PalestineRemix/mobile/
-# scp -r -i .ssh/id_rsa mobile-T/* www-data@aljazeera.io:/var/www/html/ajt/PalestineRemix/mobile/
 
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i .ssh/id_rsa"  mobile-E/* www-data@aljazeera.io:/var/www/html/aje/PalestineRemix/mobile/
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i .ssh/id_rsa"  mobile-A/* www-data@aljazeera.io:/var/www/html/aja/PalestineRemix/mobile/
