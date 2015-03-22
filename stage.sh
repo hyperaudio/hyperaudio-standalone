254d5b54090898a258b376096d69e853ef153629
Laurian Gridinoc <laurian@gridinoc.name>
2015-03-22 05:49:38 -0300

    deployment script + ssh keys

---
+++ stage.sh
@@ -0,0 +1,4 @@
+scp -r -i .ssh/id_rsa mobile-E/* www-data@aljazeera.io:/var/www/html/aje/PalestineRemix/mobile/
+scp -r -i .ssh/id_rsa mobile-A/* www-data@aljazeera.io:/var/www/html/aja/PalestineRemix/mobile/
+scp -r -i .ssh/id_rsa mobile-B/* www-data@aljazeera.io:/var/www/html/ajb/PalestineRemix/mobile/
+scp -r -i .ssh/id_rsa mobile-T/* www-data@aljazeera.io:/var/www/html/ajt/PalestineRemix/mobile/
