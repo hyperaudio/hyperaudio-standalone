83db7be78e820c69f6b2df6287cc5cace7b9f981
Laurian Gridinoc <laurian@gridinoc.name>
2015-10-31 15:30:22 +0300

    share...

--- php/head-B.php
+++ php/head-B.php
@@ -1,9 +1,11 @@
 <?php
-$locale = "bs_BA"; 
+$locale = "bs_BA";
 $L = "B";
 $title = "PalestineRemix";
 $description = "Explore the story of Palestine. Take our content and use our creative remix tool to re-edit our films. Create your own palestine story";
 $images = "http://interactive.aljazeera.com/aje/PalestineRemix/transcripts/images/";
+$image = $images . "default.png";
+$TROOT = "transcripts/html/";
 
 $titles = [
 "Going against the grain",
@@ -40,31 +42,46 @@ $titles = [
 $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $url = substr($url, 0, strpos($url, "?"));
 
-$image = $images . "default.png";
+function get_include_contents($filename) {
+    if (is_file($filename)) {
+        ob_start();
+        include $filename;
+        return ob_get_clean();
+    }
+    return false;
+}
 
 if ($_GET["_escaped_fragment_"]) {
   $frag = explode('/', urldecode($_GET["_escaped_fragment_"]));
 
+  $vid = null;
+  $t = null;
+  $t2 = null;
+
   if (!strpos(urldecode($_GET["_escaped_fragment_"]), ':')) {
     // transcript
     $v = $frag[1];
-    $t = floor($frag[2] / 1000);
-    $image = $images . $v . "/" . $L . "/p/img" . $t . ".jpg";
-    $title .= " - " . $titles[$v];
+    $vid =$v;
+    $t = $frag[2];
+    $ts = floor($t / 1000);
+    $t2 = $frag[3];
+    $image = $images . $v . "/" . $L . "/p/img" . $ts . ".jpg";
+    $title = "Isječak iz Palestina Remix - " . $titles[$v];
   } else {
     // mix
-    $vid = null;
     for ($i = 1; $i < count($frag); $i++) {
       $vt = explode(":", $frag[$i]);
       $v = $vt[0];
       if (!is_numeric($v)) continue;
       $vid = $v;
-      $t = floor($vt[2] / 1000);
-      $image = $images . $v . "/" . $L . "/p/img" . $t . ".jpg";
+      $t = explode(",", $vt[1])[0];
+      $ts = floor($t / 1000);
+      $t2 = explode(",", $vt[1])[1];
+      $image = $images . $v . "/" . $L . "/p/img" . $ts . ".jpg";
       break;
     }
 
-    if ($vid) $title .= " - " . $titles[$vid];
+    if ($vid) $title = "Remix iz Palestina Remix - " . $titles[$vid];
 
     for ($i = 1; $i < count($frag); $i++) {
       $vt = explode(":", $frag[$i]);
@@ -73,14 +90,33 @@ if ($_GET["_escaped_fragment_"]) {
         $title = explode(",", $vt[1])[0];
       }
     }
+    $t2 = $t + $t2;
+  } // mix
+
+  $transcript = get_include_contents($TROOT . $L . '/' . $vid . '.html');
+  $description = "";
+
+  $lastPara = "";
+
+  require('phpQuery/phpQuery.php');
+  $doc = phpQuery::newDocument($transcript);
+  phpQuery::selectDocument($doc);
+  $words = pq('article')['a'];
+  foreach($words as $word) {
+    $tw = pq($word)->attr("data-m");
+    if ($tw >= $t && $tw <= $t2) {
+      //$description .= pq($word)->text();
+      $para = pq($word)->parent()->text();
+      if ($para != $lastPara) {
+        $description .= str_replace("\n", '', $para);
+        $lastPara = $para;
+      }
+    }
   }
 
-
-
-  // $list = file_get_contents("list.json");
-
   $url .= '#!' . urldecode($_GET["_escaped_fragment_"]);
-}
+} // _escaped_fragment_
+
 ?>
 
 <meta property="og:locale" content="<?php echo htmlspecialchars($locale); ?>" />
